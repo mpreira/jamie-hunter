5d7e9e025ee4aebb01b1dc35ac3defb3fcd32f06
Blondel Romain <66734494+Boma1n@users.noreply.github.com>
2020-12-06 18:33:18 +0100

    Maj-Error
    Correction of Romain's error

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import grid from "./grid.js";
 import navbar from "./navbar.js";
 import foot from "./footer.js";
 import like from "./like.js";
+import comments from './comments.js';
 //import slidershow from './slidershow.js';
 //import slidershow from'./slider.vue';
 
@@ -10,6 +11,7 @@ new Vue({
   el:'#app',
 
   components: {
+    comments,
     grid,
     navbar,
     foot,
