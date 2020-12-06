ff714e17c878521b297259c558678b6063212d74
Sarah <sa.tahiris@gmail.com>
2020-12-06 13:07:03 +0100

    modif navbar

--- js/navbar.js
+++ js/navbar.js
@@ -4,9 +4,9 @@ export default Vue.component('navbar', {
     <nav class="topnav">
 
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
             <li><a href="#">Contact</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="index.html">Home</a></li>
         </ul>
     </nav>
     `,
