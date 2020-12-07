9f4b19c82114b846f1185954118887f91dcab06a
Marie-Louise Preira <65363552+mpreira@users.noreply.github.com>
2020-12-07 19:05:42 +0100

    v

--- js/grid.js
+++ js/grid.js
@@ -2,12 +2,15 @@ export default Vue.component('grid', {
 
   template: `
   <div class="image-container">
-  <img src="img/cactus.jpg" class="img1">
-  <img src="img/flamant.jpg" class="img2">
-  <img src="img/maison.jpg" class="img3">
-  <img src="img/mer.jpg" class="img4">
-  <img src="img/archi.jpg" class="img5">
-  <img src="img/green.jpg" class="img6">
+  <a href="#.html"><img src="img/cactus.jpg" class="img1"></a>
+  <a href="#.html"><img src="img/flamant.jpg" class="img2"></a>
+  <a href="#.html"><img src="img/maison.jpg" class="img3"></a>
+  <a href="#.html"><img src="img/mer.jpg" class="img4"></a>
+  <a href="#.html"><img src="img/archi.jpg" class="img5"></a>
+  <a href="#.html"><img src="img/green.jpg" class="img6"></a>
+  <a href="#.html"><img src="img/car.jpg"></a>
+  <a href="#.html"><img src="img/orange.jpg"></a>
+  <a href="#.html"><img src="img/peinture_rose.jpg"></a>
   </div>
   `,
 
