2953809ef8dd09b25e4474032cc503f30d27529f
Sarah <sa.tahiris@gmail.com>
2020-12-07 20:43:37 +0100

    derniere fois

--- js/footer.js
+++ js/footer.js
@@ -2,20 +2,20 @@ export default Vue.component('foot', {
 
   template: `
       <footer>
-    
+        <section>
         <article>
              <a href="https://unsplash.com/"> Copyright &copy; 2020 photos: All rights reserved by Unsplash</a>
              <p> Raphaelle Clerici . Hatim El Hissi . Marie-Louise Preira . Romain Blondel . Sarah Tahiri </p>
              <a href="https://github.com/mpreira/jamie-hunter">Github repository</a>
         </article>
-    
+
         <article>
             <a href="#"><i class="fab fa-facebook"></i></a>
             <a href="#"><i class="fab fa-instagram"></i></a>
             <a href="#"><i class="fab fa-twitter"></i></a>
             <a href="#"><i class="fab fa-pinterest"></i></a>
         </article>
-    
+        </section>
     </footer>
   `,
 
