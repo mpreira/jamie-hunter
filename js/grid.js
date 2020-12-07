c9bc39ace4f0b1b8c7712fe0e5e3faee583c6eaf
Sarah <sa.tahiris@gmail.com>
2020-12-07 19:06:43 +0100

    accord des pages

--- js/grid.js
+++ js/grid.js
@@ -2,12 +2,12 @@ export default Vue.component('grid', {
 
   template: `
   <div class="image-container">
-  <img src="img/cactus.jpg" class="img1">
-  <img src="img/flamant.jpg" class="img2">
-  <img src="img/maison.jpg" class="img3">
-  <img src="img/mer.jpg" class="img4">
-  <img src="img/archi.jpg" class="img5">
-  <img src="img/green.jpg" class="img6">
+  <a class="img1" href="../cactus.html"><img src="img/cactus.jpg" ></a>
+   <a class="img2" href="../flamant.html"><img src="img/flamant.jpg" ></a>
+   <a class="img3" href="../maison.html"><img src="img/maison.jpg" ></a>
+   <a class="img4" href="../mer.html"><img src="img/mer.jpg" ></a>
+   <a class="img5" href="../archi.html"><img src="img/archi.jpg" ></a>
+   <a class="img6" href="../green.html"><img src="img/green.jpg" ></a>
   </div>
   `,
 
